84b006aa09140085cfc61fd58c920a40eafdc8a8
Lucas <118465614+LucasTHERON@users.noreply.github.com>
2024-07-30 00:21:04 +0200

    Update crud.svelte
    Update is almost there

--- crud.svelte
+++ crud.svelte
@@ -9,7 +9,7 @@
     { id: 4, name: "louis", city: "marseille" },
   ];
 
-  let nameToAdd, cityToAdd;
+  let nameToAdd, nameToUpdate, cityToAdd, cityToUpdate;
 
   function addData() {
     data.push({ id: data.length, name: nameToAdd, city: cityToAdd });
@@ -19,6 +19,11 @@
     data.splice(i, 1);
 		data = data;
   }
+
+	function updateData(i) {
+    data[i] = { id: i, name: "new", city: "new york" };
+		data = data;
+  }
 </script>
 
 <h1>Hello {myNameToday}!</h1>
@@ -26,10 +31,13 @@
 {#each data as row, i}
   <p>
     <b>{row.id} :</b> {row.name} from {row.city}
-    <button on:click={() => deleteData(i)}>X</button>
-  </p>
+    <button on:click={() => deleteData(i)}>X</button><br>
+		<input type="text" bind:value={nameToUpdate}>
+		<input type="text" bind:value={cityToUpdate}>
+    <button on:click={() => updateData(i)}>Modifer</button><br>
+  </p><hr>
 {/each}
-
+<h3>Ajouter une ligne</h3>
 <input type="text" bind:value={nameToAdd}>
 <input type="text" bind:value={cityToAdd}>
 
